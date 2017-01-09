f5d203025e50503a77504302b9a337955ff8c4ee
IGolovach <igolovach@example.com>
2017-01-09 06:42:48 +0200

    photos, album, photo view

--- js/allPhotos.js
+++ js/allPhotos.js
@@ -1,8 +1,23 @@
 var app = angular.module('picViewApp', []);
-
 app.controller('allPhotos', function allPhotos($scope, $http) {
     $http.get('http://jsonplaceholder.typicode.com/photos').
         then(function(response) {
-            $scope.photos = response.data;
-        });
+            $scope.allphotos = response.data;
+	});
+	$scope.getAlbum = function(albumId){
+		$http.get('http://jsonplaceholder.typicode.com/album/'+albumId+'/photos/').
+        then(function(response) {
+			$scope.photos = 0;
+            return $scope.albumPhoto = response.data;
+        }); 
+	};	
+	$scope.getPhoto = function(id){
+		$http.get('http://jsonplaceholder.typicode.com/photos/'+id).
+        then(function(response) {
+			$scope.allphotos = 0;
+			$scope.albumPhoto = 0;
+            return $scope.onephoto = response.data;
+        }); 
+	};
 });
+
